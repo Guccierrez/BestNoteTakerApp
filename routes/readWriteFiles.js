838f7610db9b8c79c0c08a515dd53b63afa62bac
Fabian Gutierrez <fabiangutierrez580@gmail.com>
2022-10-02 21:46:51 -0500

    got the delete and post working without refresh

---
+++ routes/readWriteFiles.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const util = require('util');
+
+// Promise version of fs.readFile
+const readFromFile = util.promisify(fs.readFile);
+
+
+const writeToFile = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+  );
+
+
+const readAndAppend = (content, file) => {
+  fs.readFile(file, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      parsedData.push(content);
+      writeToFile(file, parsedData);
+    }
+  });
+};
+
+const readAndDelete = (file, id) => {
+  fs.readFile(file, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+
+      const parsedData = JSON.parse(data)
+
+     
+
+        const filteredData = parsedData.filter((note) => note.id !== id);
+        // console.log("filter", typeof filteredData)
+ 
+      writeToFile(file, filteredData);
+    }
+  });
+};
+
+
+
+module.exports = { readFromFile, writeToFile, readAndAppend, readAndDelete };
